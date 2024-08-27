03571d231760428d3b6d1b61394b0ce595f67078
James Luu <jamesluu0917@gmail.com>
2024-08-26 23:07:22 -0400

    Added labels/inputs on new reservation form page

--- hotel-app/src/app/reservation/reservation.module.ts
+++ hotel-app/src/app/reservation/reservation.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    ReservationFormComponent,
-    ReservationListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ReservationFormComponent, ReservationListComponent],
+  // ReactiveFormsModule validates form in ts class
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
-export class ReservationModule { }
+export class ReservationModule {}

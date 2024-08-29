a36e4022871d37819ba9a5521f4809253965b17d
James Luu <jamesluu0917@gmail.com>
2024-08-28 21:11:58 -0400

    Added ID field and Edit router link

--- hotel-app/src/app/reservation/reservation.module.ts
+++ hotel-app/src/app/reservation/reservation.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ReservationFormComponent, ReservationListComponent],
   // ReactiveFormsModule validates form in ts class
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
 })
 export class ReservationModule {}
